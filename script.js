026f261c074afbc5f0e9e74f000e2ce2c6e2ff9f
unknown <jamesabreus@yahoo.com>
2022-04-28 18:13:49 -0400

    lightbulb-HW

---
+++ script.js
@@ -0,0 +1,14 @@
+const lightBulb = document.querySelector('lightBulb')
+const btn = document.querySelector('.btn')
+
+
+
+
+// test
+theButton.onclick = function pictureChange()
+{
+document.getElementById('theImage').src="https://toppng.com/uploads/preview/light-bulb-png-transparent-light-bulb-115628766421gdl0skp2k.png";
+}
+
+
+// test
